17d5145fbb561f5ef0457ead3645a3f29d300a9e
karla-walker <indiablonix@gmail.com>
2021-05-15 03:45:11 -0700

    Set job text input to not display on page load and added event listener that updates text input display based on selection

--- js/script.js
+++ js/script.js
@@ -1,4 +1,24 @@
-const userName = document.querySelector('#name');
+const nameInput = document.querySelector('#name');
+const otherJobInput = document.querySelector('#other-job-role');
+const jobRoleSelect = document.querySelector('#title');
+console.log(jobRoleSelect);
 
 // add default focus state
-userName.focus();
+nameInput.focus();
+
+// hide job text input on page load
+otherJobInput.style.display = 'none';
+
+// when select element detects 'change', update job text input to display/hide based on selection
+function displayTextField(option) {
+  if (option === 'other') {
+    otherJobInput.style.display = '';
+  } else {
+    otherJobInput.style.display = 'none';
+  }
+}
+
+jobRoleSelect.addEventListener('change', e => {
+  const selectedOption = e.target.selectedOptions[0].value;
+  displayTextField(selectedOption);
+});
