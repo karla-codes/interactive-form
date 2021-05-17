238f57f7cb322170d970d189d54e0f3514c31f4d
karla-walker <indiablonix@gmail.com>
2021-05-17 04:02:11 -0700

    Added form validation

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
+const form = document.querySelector('form');
 const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
 const otherJobInput = document.querySelector('#other-job-role');
 const jobRoleSelect = document.querySelector('#title');
 const colorSelect = document.querySelector('#color');
@@ -6,6 +8,7 @@ const designSelect = document.querySelector('#design');
 const activityFieldset = document.querySelector('#activities');
 const totalActivityCost = document.querySelector('#activities-cost');
 const paymentMethodSelect = document.querySelector('#payment');
+const paymentOptions = paymentMethodSelect.querySelectorAll('option');
 const creditCardOption = document.querySelector(
   '#payment [value="credit-card"]'
 );
@@ -78,9 +81,8 @@ paypalInfo.hidden = true;
 bitcoinInfo.hidden = true;
 
 // when a payment method is selected, show selected payment info and hide the rest
-function updatePaymentMethod() {
-  const paymentOptions = paymentMethodSelect.querySelectorAll('option');
 
+function updatePaymentMethod() {
   for (let i = 0; i < paymentOptions.length; i++) {
     const paymentOption = paymentOptions[i];
     if (paymentOption.selected) {
@@ -101,6 +103,74 @@ function updatePaymentMethod() {
   }
 }
 
+// validate form
+// 1. name field can't be blank/empty
+// 2. email field must be formated like one: name@email.com
+// 3. must register for at least 1 activity
+// 4. FOR CC PAYMENT ONLY (selected):
+//  -cc number must be 13-16 digits w/no dashes or spaces
+//  -zip code must contain 5 digits
+//  -CVV must contain 3 digits
+
+// validates name field
+const validateName = () => {
+  const nameRegex = /^.+$/;
+  if (nameRegex.test(nameInput.value)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+// validates email field
+const validateEmail = () => {
+  const emailRegex = /^.+@.+\.com$/;
+  if (emailRegex.test(emailInput.value)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+// validates activities
+const validateActivities = () => {
+  const activities = activityFieldset.querySelectorAll('input');
+  for (let i = 0; i < activities.length; i++) {
+    if (activities[i].checked) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+};
+
+// validates credit card IF selected
+const validateCreditCard = () => {
+  const creditCardOption = paymentOptions[1].selected;
+  const ccNumber = Number(document.querySelector('#cc-num').value);
+  const zipCode = Number(document.querySelector('#zip').value);
+  const cvv = Number(document.querySelector('#cvv').value);
+
+  if (creditCardOption) {
+    const ccNumberRegex = /\b\d{13,16}\b/;
+    const zipCodeRegex = /^\d{5}$/;
+    const cvvRegex = /^\d{3}$/;
+
+    if (
+      ccNumberRegex.test(ccNumber) &&
+      zipCodeRegex.test(zipCode) &&
+      cvvRegex.test(cvv)
+    ) {
+      return true;
+    } else {
+      console.log(typeof ccNumber, typeof zipCode, typeof cvv);
+      return false;
+    }
+  } else {
+    return true;
+  }
+};
+
 // event listeners
 jobRoleSelect.addEventListener('change', e => {
   const selectedOption = e.target.selectedOptions[0].value;
@@ -119,3 +189,16 @@ activityFieldset.addEventListener('change', e => {
 });
 
 paymentMethodSelect.addEventListener('change', updatePaymentMethod);
+
+form.addEventListener('submit', e => {
+  if (
+    validateName() &&
+    validateEmail() &&
+    validateActivities() &&
+    validateCreditCard()
+  ) {
+    return;
+  } else {
+    e.preventDefault();
+  }
+});
